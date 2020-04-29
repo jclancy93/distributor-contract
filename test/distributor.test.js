ff3a6148cec7e77dfbd01c7d0197de0ee38e8d14
Dan Octavian <danoctavian91@gmail.com>
2020-04-29 15:58:32 +0800

    add contract initialization

--- test/distributor.test.js
+++ test/distributor.test.js
@@ -8,20 +8,57 @@ const ExchangeMock = contract.fromArtifact('ExchangeMock');
 const ExchangeMKRMock = contract.fromArtifact('ExchangeMock');
 const NXMToken = contract.fromArtifact('NXMToken');
 const NXMaster = contract.fromArtifact('NXMaster');
-
+const Claims = contract.fromArtifact('Claims');
+const ClaimsData = contract.fromArtifact('ClaimsDataMock');
+const ClaimsReward = contract.fromArtifact('ClaimsReward');
+const MCR = contract.fromArtifact('MCR');
+const TokenData = contract.fromArtifact('TokenDataMock');
+const TokenFunctions = contract.fromArtifact('TokenFunctionMock');
+const TokenController = contract.fromArtifact('TokenController');
+const Pool1 = contract.fromArtifact('Pool1Mock');
+const Pool2 = contract.fromArtifact('Pool2');
+const PoolData = contract.fromArtifact('PoolDataMock');
+const Quotation = contract.fromArtifact('Quotation');
+const QuotationDataMock = contract.fromArtifact('QuotationDataMock');
+const Governance = contract.fromArtifact('Governance');
+const ProposalCategory = contract.fromArtifact('ProposalCategory');
+const MemberRoles = contract.fromArtifact('MemberRoles');
 const Distributor = contract.fromArtifact('Distributor');
 
 
 const INITIAL_SUPPLY = '1500000000000000000000000';
 
+const EXCHANGE_TOKEN = '10000000000000000000000';
+const EXCHANGE_ETHER = 10 ** 19;
+
 async function setup () {
 
-  this.founderAddress = accounts[0];
+  const founderAddress = accounts[0];
 
-  this.nxmToken = await NXMToken.new(this.founderAddress, INITIAL_SUPPLY);
-  this.nxMaster = await NXMaster.new(this.nxmToken.address);
-  this.distributorFeePercentage = 10;
-  this.distributor = await Distributor.new(this.nxMaster.address, this.distributorFeePercentage);
+  const dai = await DAI.new();
+  const mkr = await MKR.new();
+  await DSValue.new(founderAddress);
+  const factory = await FactoryMock.new();
+  const exchange = await ExchangeMock.new(
+    dai.address,
+    factory.address
+  );
+  const exchangeMKR = await ExchangeMKRMock.new(
+    mkr.address,
+    factory.address
+  );
+
+  await factory.setFactory(dai.address, exchange.address);
+  await factory.setFactory(mkr.address, exchangeMKR.address);
+  await dai.transfer(exchange.address, EXCHANGE_TOKEN);
+  await mkr.transfer(exchangeMKR.address, EXCHANGE_TOKEN);
+  await exchange.recieveEther({ value: EXCHANGE_ETHER });
+  await exchangeMKR.recieveEther({ value: EXCHANGE_ETHER });
+
+  const nxmToken = await NXMToken.new(founderAddress, INITIAL_SUPPLY);
+  const nxMaster = await NXMaster.new(nxmToken.address);
+  const distributorFeePercentage = 10;
+  const distributor = await Distributor.new(nxMaster.address, distributorFeePercentage);
 
 }
 
