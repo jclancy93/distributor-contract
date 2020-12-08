4605579be9331b2a4d00e2831c431c53740d43de
Dan Octavian <danoctavian91@gmail.com>
2020-12-08 15:41:46 +0800

    add auxiliary contracts used in test setup

---
+++ contracts/external/ImportNexusMutual.sol
@@ -0,0 +1,30 @@
+/* Copyright (C) 2020 NexusMutual.io
+  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ */
+
+pragma solidity >0.5.0;
+
+import "../../nexusmutual-contracts/contracts/modules/capital/Pool.sol";
+import "../../nexusmutual-contracts/contracts/modules/governance/external/OwnedUpgradeabilityProxy.sol";
+import "../../nexusmutual-contracts/contracts/modules/claims/ClaimProofs.sol";
+
+import "../../nexusmutual-contracts/contracts/mocks/ERC20Mock.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Pool/P1MockChainlinkAggregator.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposableNXMaster.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposableMemberRoles.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposableTokenController.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposableProposalCategory.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposableGovernance.sol";
+import "../../nexusmutual-contracts/contracts/mocks/Disposables/DisposablePooledStaking.sol";
+
+import "../../nexusmutual-contracts/contracts/external/WETH9.sol";
+
+interface ImportNexusMutual {}
---
+++ contracts/external/ImportUniswapPeriphery.sol
@@ -0,0 +1,5 @@
+
+
+import "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
+
+interface ImportUniswapPeriphery {}
