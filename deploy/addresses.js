b65275a9e30f455a6a5fe32ae842e82eb433cd71
Dan Octavian <danoctavian91@gmail.com>
2021-03-05 14:47:38 +0800

    add addresses file

---
+++ deploy/addresses.js
@@ -0,0 +1,14 @@
+const FACTORY = {
+  'kovan': '0xB2683eA644744FdD858E1Da0F43A6aa3B12d6183',
+  'mainnet': undefined
+}
+
+const MASTER = {
+  'kovan': '0xf3b1018c6f1d5c2beCA1ebB4C7Bcf472D6721501',
+  'mainnet': undefined
+}
+
+module.exports = {
+  FACTORY,
+  MASTER
+}
